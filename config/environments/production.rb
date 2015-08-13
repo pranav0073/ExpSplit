926227bbeb851d5914a3c9a2c12a38e8bd3cbe62
pranav <pranav0073@gmail.com>
2015-08-13 23:21:46 +0530

    dasda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-
+  Rails.application.routes.default_url_options[:host] = 'https://damp-plains-9470.herokuapp.com'
   config.action_mailer.default_url_options = { :host => 'https://damp-plains-9470.herokuapp.com' }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
